f7c8996d5904e4d11b1f74c48889ad132f97887b
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2023-10-20 00:31:59 +0200

    add content on message and a toString method

--- main/src/features/message/Message.java
+++ main/src/features/message/Message.java
@@ -1,20 +1,36 @@
 package features.message;
 
 import features.contact.*;
-
+import features.profile.*;
 import static features.message.MessageState.*;
 
 public class Message {
-    Contact sender;
+    Profile sender;
     Contact receiver;
+    String content;
     MessageState state;
 
-    public Message(Contact sender, Contact receiver){
+    //TODO add conversation to know in which conversation we are
+    public Message(Profile sender, Contact receiver, String content) {
         this.sender = sender;
         this.receiver = receiver;
+        this.content = content;
         this.state = sending;
         this.send();
     }
+
+    public Profile getSender() {
+        return this.sender;
+    }
+
+    public Contact getReceiver() {
+        return this.receiver;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
 
     public void send(){
         //TODO send the message to the receiver user
@@ -27,5 +43,11 @@ public class Message {
     }
 
     public void seen(){
+        // TODO logic when receiver has read the message
+    }
+
+    public String toString(){
+        return this.sender.getName() + " send to " + this.receiver.getName()
+                + ": " + this.content + " -> " + this.state;
     }
 }
