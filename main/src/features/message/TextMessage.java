4f811fa4255b2d3b6355ff2ff996eab98d193bec
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2023-10-20 14:02:52 +0200

    Add a conversation in Message

--- main/src/features/message/TextMessage.java
+++ main/src/features/message/TextMessage.java
@@ -1,12 +1,13 @@
 package features.message;
 
 import features.contact.Contact;
+import features.conversation.Conversation;
 import features.profile.Profile;
 
 public class TextMessage extends Message{
     String content;
-    public TextMessage(Profile sender, Contact receiver, String content){
-        super(sender, receiver);
+    public TextMessage(Contact sender, Contact receiver, String content, Conversation conversation){
+        super(sender, receiver, conversation);
         this.content = content;
     }
 
