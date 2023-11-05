81ca745b8b70178a032159ddbc33d3182787570a
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2023-11-05 16:32:34 +0100

    activate deactivate feature

--- main/src/commands/Activate.java
+++ main/src/commands/Activate.java
@@ -1,6 +1,8 @@
 package commands;
 
 import context.Context;
+import database.Features;
+import features.FeatureBehavior;
 
 import java.util.List;
 
@@ -14,11 +16,43 @@ public class Activate extends Command{
     @Override
     public void run() {
         List<String> args = this.getArguments(true);
-        for(String arg: args) {
-            int index = contexts.stream().map(Context::getName).toList().indexOf(arg);
-            if(index != -1){
-                contexts.get(index).activate();
-            }else Context.error(arg + " not found!");
+        if(args.size() < 2) {
+            error("need at least 2 arguments: context/feature and then the context or feature name to activate");
+        }
+        String key = args.get(0);
+        args.remove(0);
+        if(key.equals("context")) {
+            for (String arg : args) {
+                activateContext(arg);
+            }
+        }else if(key.equals("feature")){
+            for(String arg: args) {
+                activateFeature(arg);
+            }
+        }else error("Not recognized argument: " + key);
+    }
+
+    public void activateContext(String arg){
+        arg = arg.replace(",", "");
+        int index = contexts.stream().map(Context::getName).toList().indexOf(arg);
+        if (index != -1) {
+            contexts.get(index).activate();
+        } else Context.error(arg + " not found!");
+    }
+
+    public void activateFeature(String arg){
+        FeatureBehavior behavior = Features.INSTANCE.get(arg);
+        if(behavior == null){
+            error(arg + " is not a feature");
+            return;
+        }
+        // TODO list alternative to check if activated and deactivate them
+        if(behavior.getMandatory()) error("Mandatory features are always activated");
+        else if(behavior.getOptional()){
+            if(!behavior.isActivated()){
+                behavior.activate();
+                feedback(arg + " is now activated." );
+            }else error(arg + " is already activated!");
         }
     }
 
--- main/src/commands/Deactivate.java
+++ main/src/commands/Deactivate.java
@@ -1,11 +1,12 @@
 package commands;
 
 import context.Context;
+import database.Features;
+import features.FeatureBehavior;
 
 import java.util.List;
 
 public class Deactivate extends Command {
-
     public Deactivate(List<Context> contexts, String command){
         super("deactivate", contexts, command);
     }
@@ -13,12 +14,30 @@ public class Deactivate extends Command {
     @Override
     public void run() {
         List<String> args = this.getArguments(true);
-        for(String arg: args) {
-            int index = contexts.stream().map(Context::getName).toList().indexOf(arg);
-            if(index != -1){
-                contexts.get(index).deactivate();
-            }else Context.error(arg + " not found!");
+        if(args.size() < 2) {
+            error("need at least 2 arguments: context/feature and then the context or feature name to activate");
         }
+        String key = args.get(0);
+        args.remove(0);
+        if(key.equals("context")) {
+            for(String arg: args) {
+                int index = contexts.stream().map(Context::getName).toList().indexOf(arg);
+                if(index != -1){
+                    contexts.get(index).deactivate();
+                }else Context.error(arg + " not found!");
+            }
+        }else if(key.equals("feature")){
+            for(String arg: args) {
+                FeatureBehavior behavior = Features.INSTANCE.get(arg);
+                // TODO list alternative to check if activated and deactivate them
+                if(behavior.getOptional()){
+                    if(behavior.isActivated()){
+                        behavior.deactivate();
+                        feedback(arg + " is now deactivated." );
+                    }else error(arg + " is already deactivated!");
+                }else if(behavior.getMandatory()) error("Mandatory features can't be deactivated");
+            }
+        }else error("Not recognized argument: " + key);
     }
 
     @Override
