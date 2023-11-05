26870793b4f5d96a2520a7a91de26335782d866b
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2023-11-05 17:14:39 +0100

    Dark mode activated by default

--- main/src/database/Features.java
+++ main/src/database/Features.java
@@ -1,6 +1,7 @@
 package database;
 
 import features.FeatureBehavior;
+import features.themes.Dark;
 
 import java.util.HashMap;
 
@@ -26,6 +27,7 @@ public enum Features {
         features.get("dark").activate();
         features.get("dark").addAlternativeFeature("light");
         features.get("light").addAlternativeFeature("dark");
+        Dark.activate();
     }
 
     public HashMap<String, FeatureBehavior> get(){
