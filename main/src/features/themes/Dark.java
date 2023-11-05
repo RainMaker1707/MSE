017a23875365ff12beacc0fee537134dfcbd7a15
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2023-11-05 17:13:40 +0100

    Simulation of dark and light mode

--- main/src/features/themes/Dark.java
+++ main/src/features/themes/Dark.java
@@ -1,2 +1,16 @@
-package features.themes;public class Dark {
+package features.themes;
+
+import constant.Colors;
+import database.Features;
+import features.FeatureBehavior;
+
+public class Dark {
+    static FeatureBehavior behavior = Features.INSTANCE.get("dark");
+
+    public static void activate(){
+        if(behavior.isActivated()) {
+            System.out.print(Colors.BG_BLACK + Colors.ANSI_WHITE);
+            Colors.currentColor = Colors.ANSI_WHITE;
+        }
+    }
 }
--- main/src/features/themes/Light.java
+++ main/src/features/themes/Light.java
@@ -1,2 +1,16 @@
-package features.themes;public class Light {
+package features.themes;
+
+import constant.Colors;
+import database.Features;
+import features.FeatureBehavior;
+
+public class Light {
+    static FeatureBehavior behavior = Features.INSTANCE.get("light");
+
+    public static void activate(){
+        if(behavior.isActivated()) {
+            System.out.print(Colors.BG_WHITE + Colors.ANSI_BLACK);
+            Colors.currentColor = Colors.ANSI_BLACK;
+        }
+    }
 }
