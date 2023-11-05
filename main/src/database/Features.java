62a4dd51a81ea0895ff4aca7b1178bfc3f9e1edd
RainMaker1707 <hadrien.allegaert@student.uclouvain.be>
2023-11-05 16:31:34 +0100

    Features DB

---
+++ main/src/database/Features.java
@@ -0,0 +1,39 @@
+package database;
+
+import features.FeatureBehavior;
+
+import java.util.HashMap;
+
+public enum Features {
+
+    INSTANCE();
+
+    HashMap<String, FeatureBehavior> features = new HashMap<>();
+
+    public void populate(){
+        features.put("contact", new FeatureBehavior("contact", "mandatory"));
+        features.put("list", new FeatureBehavior("list", "mandatory"));
+        features.put("conversation", new FeatureBehavior("conversation", "mandatory"));
+        features.put("group", new FeatureBehavior("group", "mandatory"));
+        features.put("message", new FeatureBehavior("message", "mandatory"));
+        features.put("text", new FeatureBehavior("text", "mandatory"));
+        features.put("profile", new FeatureBehavior("profile", "mandatory"));
+
+        features.put("block", new FeatureBehavior("block", "optional"));
+
+        features.put("dark", new FeatureBehavior("dark", "alternative"));
+        features.put("light", new FeatureBehavior("light", "alternative"));
+        features.get("dark").activate();
+        features.get("dark").addAlternativeFeature("light");
+        features.get("light").addAlternativeFeature("dark");
+    }
+
+    public HashMap<String, FeatureBehavior> get(){
+        return features;
+    }
+
+    public FeatureBehavior get(String name){
+        return features.get(name);
+    }
+
+}
